07f4edf3e621e68ed64f2289ba74e7edc2635d15
Andrew Nagy <andrew.nagy@the159.com>
2018-09-04 18:50:00 -0400

    add rest

---
+++ Api/Rest/Daynight.php
@@ -0,0 +1,56 @@
+<?php
+namespace FreePBX\modules\Daynight\Api\Rest;
+use FreePBX\modules\Api\Rest\Base;
+class Daynight extends Base {
+	protected $module = 'daynight';
+	public function setupRoutes($app) {
+
+		/**
+		* @verb GET
+		* @returns - a list of daynight settings
+		* @uri /daynight
+		*/
+		$app->get('/', function ($request, $response, $args) {
+			\FreePBX::Modules()->loadFunctionsInc('daynight');
+			return $response->withJson(daynight_list());
+		})->add($this->checkAllReadScopeMiddleware());
+
+		/**
+		 * @verb GET
+		 * @returns - daynight state
+		 * @uri /daynight/:id
+		 */
+		$app->get('/{id}', function ($request, $response, $args) {
+			\FreePBX::Modules()->loadFunctionsInc('daynight');
+
+			$dn = new \dayNightObject($args['id']);
+
+			if ($dn) {
+				$daynight = array();
+				$daynight['state'] = $dn->getState();
+			}
+
+			$daynight = $daynight ? $daynight : false;
+
+			return $response->withJson($daynight);
+		})->add($this->checkAllReadScopeMiddleware());
+
+		/**
+		 * @verb PUT
+		 * @uri /daynight/:id
+		 */
+		$app->put('/{id}', function ($request, $response, $args) {
+			\FreePBX::Modules()->loadFunctionsInc('daynight');
+			$params = $request->getParsedBody();
+			$dn = new dayNightObject($args['id']);
+
+			if ($dn) {
+				$dn->setState($params['state']);
+
+				return $response->withJson(true);
+			}
+
+			return $response->withJson(false);
+		})->add($this->checkAllWriteScopeMiddleware());
+	}
+}
