d2f05f0b2e532a08cb20fa82b889fb18129ca3de
Franck Danard <fdanard@sangoma.com>
2019-06-20 16:16:29 +0000

    Use dumpAstDB

--- Backup.php
+++ Backup.php
@@ -2,25 +2,11 @@
 namespace FreePBX\modules\Daynight;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
-  
-	public function parse_daynight(){
-		$dn_data = $this->FreePBX->astman->database_show('DAYNIGHT');
-		$result	= array();
-		foreach($dn_data as $key => $dn){
-			$res = explode('/',$key);
-			$family = $res[1];
-			$k 		= $res[2];
-			$value	= $dn;
-			$result[] = array($family => array($k => $value));
 
-		}
-		return $result;
-	}
-  
 	public function runBackup($id,$transaction){
 		$configs = [
 			'tables' => $this->dumpTables(),
-			'astdb' => $this->parse_daynight(),
+			'astdb' => $this->dumpAstDB('DAYNIGHT'),
 			'features' => $this->dumpFeatureCodes(),
 			'settings' => $this->dumpAdvancedSettings()
 		];
